fd8bae3e68208217cdb5d5a4aa2117c7c2e5d1ea
ridoy113 <almamounhridoy113@gmail.com>
2022-07-19 16:52:54 +0600

    start whatGPT3 section

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Feature } from '../../components/indes';
 import './whatGPT3.css'
 
 
 const WhatGPT3 = () => {
     return (
-        <div>
-            WhatGPT3
+        <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+            <div className='gpt3__whatgpt3-feature'>
+                <Feature />
+            </div>
+
+            <div className='gpt3__whatgpt3-heading'>
+                <h1 className='gradient__text'>
+                    The possibilities beyond your imagination
+                </h1>
+                <p>Explore The Library</p>
+            </div>
+
+
+            <div className='gpt3__whatgpt3-container'>
+                <Feature/>
+                <Feature/>
+                <Feature/>
+            </div>
         </div>
     );
 };
