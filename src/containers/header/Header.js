dcdeb6858d0d2b01ec2004f89dbba908bff63191
ridoy113 <almamounhridoy113@gmail.com>
2022-07-19 16:26:23 +0600

    complte header

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './header.css'
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+
 
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='gpt3__header section__padding' id='home'>
+            <div className='gpt3__header-content'>
+                <h1 className='gradient__text'>
+                    Let's Build Something amazing with GPT-3 OpenAI
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quae earum, reprehenderit veritatis ipsum vel impedit sint, laboriosam voluptatum voluptatibus enim qui incidunt veniam libero quos minima? Iure, sint unde?</p>
+
+                <div className='gpt3__header-content__input'>
+                    <input type="email" placeholder='Your Email Adders' />
+                    <button type='button'>Get Started</button>
+                </div>
+
+                <div className='gpt3__header-content__people'>
+                    <img src={people} alt="people" />
+                    <p>1,600 people requested access a visit in ast 24 hours.</p>
+                </div>
+            </div>
+            <div className='gpt3__header-image'>
+                <img src={ai} alt="ai" />
+            </div>
         </div>
     );
 };
