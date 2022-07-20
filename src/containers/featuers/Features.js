1a41faf26861713fbff2f4d39d1e0a4d303326c8
ridoy113 <almamounhridoy113@gmail.com>
2022-07-20 12:31:07 +0600

    start feature section

--- src/containers/featuers/Features.js
+++ src/containers/featuers/Features.js
@@ -4,8 +4,10 @@ import './Features.css'
 
 const Features = () => {
     return (
-        <div>
-            
+        <div className='gpt3__features section__padding' id='features'>
+            <div className='gpt3__features-heading'>
+                <h1 className='gradient__text'>The Feature is Now and You Just Need To Realize It. Step into Future Today & Make it Happy</h1>
+            </div>
         </div>
     );
 };
