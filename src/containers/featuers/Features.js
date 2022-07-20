210a3aa4e3876f97c25b8ab1639d7df5c6128f59
ridoy113 <almamounhridoy113@gmail.com>
2022-07-20 13:01:25 +0600

    complete feature section

--- src/containers/featuers/Features.js
+++ src/containers/featuers/Features.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Feature } from '../../components/indes';
 import './Features.css'
+
+
+const featuresData = [
+    {
+        title: 'Improving end distrusts instantly',
+        text: 'Independent developers have also released many unofficial apps for reading Wikipedia articles. Some apps load content from the Wikipedia site and process it; other apps use the MediaWiki API.'
+    },
+    {
+        title: 'Become the tended active',
+        text: 'Independent developers have also released many unofficial apps for reading Wikipedia articles. Some apps load content from the Wikipedia site and process it; other apps use the MediaWiki API.'
+    },
+    {
+        title: 'Message or am nothing',
+        text: 'Independent developers have also released many unofficial apps for reading Wikipedia articles. Some apps load content from the Wikipedia site and process it; other apps use the MediaWiki API.'
+    },
+    {
+        title: 'Really boy law count',
+        text: 'Independent developers have also released many unofficial apps for reading Wikipedia articles. Some apps load content from the Wikipedia site and process it; other apps use the MediaWiki API.'
+    },
+]
 
 
 const Features = () => {
@@ -7,6 +28,13 @@ const Features = () => {
         <div className='gpt3__features section__padding' id='features'>
             <div className='gpt3__features-heading'>
                 <h1 className='gradient__text'>The Feature is Now and You Just Need To Realize It. Step into Future Today & Make it Happy</h1>
+                <p>Request Early Access to Get Started</p>
+            </div>
+
+            <div className='gpt3__features-container'>
+                {featuresData.map((item, index) => (
+                    <Feature title={item.title} text={item.text} key={item.title + index} />
+                ))}
             </div>
         </div>
     );
